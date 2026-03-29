13a86ad8148d8ca21c00176bed763a3d443d3d20
Richard VCD <richardvincentius2112@gmail.com>
2026-03-29 18:04:15 +0700

    Update file index.balde.php
    Bisa hover ke app sosial media tampilannya

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -86,6 +86,15 @@
             </div>
         </div>
         <div class="member-item set-bg" data-setbg="{{ asset('user/img/team-member/member-1.jpg') }}">
+            <div class="mi-social">
+                <div class="mi-social-inner bg-gradient">
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                </div>
+            </div>
+
             <div class="mi-text">
                 <h5>Emma Sandoval</h5>
                 <span>Speaker</span>
