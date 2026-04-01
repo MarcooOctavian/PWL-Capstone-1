f980f57e93f26cf5e5a168f44567e5ac5aa7fa31
kevin-krm <kevinkornelius.kk@gmail.com>
2026-04-01 08:51:51 +0700

    Perbaikan Register & Login
    Sekarang mengarah ke tabael users dengan database baru

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,7 +32,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:100'],
-            'email' => ['required', 'string', 'email', 'max:100', 'unique:user,email'],
+            'email' => ['required', 'string', 'email', 'max:100', 'unique:users,email'],
             'password' => ['required', 'confirmed', 'min:6'],
             'phone' => ['nullable', 'string', 'max:15'],
         ]);
